f9bc47cb70fa5e91361610969cfa5f8f117b7579
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-27 23:37:35 +0100

    Adding states to inputs on SignUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
@@ -8,15 +9,29 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
 
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
   }
+
+  function handleSignUp() {
+    console.log({
+      name,
+      email,
+      password,
+      passwordConfirm
+    });
+  }
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-      <VStack flex={1} px='10' pb='16'>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
           defaultSource={BackgroundImg}
@@ -34,31 +49,41 @@ export function SignUp() {
         </Center>
 
         <Center>
-          <Heading color='gray.100' fontSize='xl' mb={6} fontFamily="heading">
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
             Crie sua conta
           </Heading>
 
           <Input
             placeholder="Nome"
+            onChangeText={setName}
           />
 
           <Input
             placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
+            onChangeText={setEmail}
           />
-
           <Input
             placeholder="Senha"
             secureTextEntry
+            onChangeText={setPassword}
+          />
+          <Input
+            placeholder="Confirmar a Senha"
+            secureTextEntry
+            onChangeText={setPasswordConfirm}
           />
 
-          <Button title="Criar e acessar" />
+          <Button
+            title="Criar e acessar"
+            onPress={handleSignUp}
+          />
         </Center>
 
         <Button
           title="Voltar para o login"
-          variant='outline'
+          variant="outline"
           mt={24}
           onPress={handleGoBack}
         />
