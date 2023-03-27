803b5a1e8620c8447a0bd7890a83c5830a66e24a
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-27 18:57:16 +0100

    Finished styles in Bottom Tabs/ Hide unused screen

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import { useTheme } from 'native-base';
 import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
@@ -31,6 +32,13 @@ export function AppRoutes() {
       tabBarShowLabel: false,
       tabBarActiveTintColor: colors.green[500],
       tabBarInactiveTintColor: colors.gray[200],
+      tabBarStyle: {
+        backgroundColor: colors.gray[600],
+        borderTopWidth: 0,
+        height: Platform.OS === 'android' ? 'auto' : 96,
+        paddingBottom: sizes[10],
+        paddingTop: sizes[6],
+      }
     }}>
       <Screen
         name='home'
@@ -77,6 +85,9 @@ export function AppRoutes() {
       <Screen
         name='exercise'
         component={Exercise}
+        options={{
+          tabBarButton: () => null,
+        }}
       />
 
     </Navigator>
