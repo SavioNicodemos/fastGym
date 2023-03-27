3b640df08163c7df26098a1962db9bc418815d46
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-27 20:17:53 +0100

    Finished HomeHeader/ NatBase custom Icon

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,17 +1,37 @@
-import { Heading, HStack, Text, VStack } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Heading, HStack, Text, VStack, Icon } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
   return (
-    <HStack>
-      <VStack>
-        <Text color="gray.100">
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: 'https://github.com/savionicodemos.png' }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
+        <Heading color="gray.100" fontSize="md">
           Nicodemos
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon
+          as={MaterialIcons}
+          name='logout'
+          color='gray.100'
+          size={7}
+        />
+      </TouchableOpacity >
     </HStack>
   );
 }
