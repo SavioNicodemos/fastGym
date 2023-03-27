e03b7f08f8c6cac12d8d91977310bb608e0b213d
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-27 22:05:51 +0100

    Fix: Visual bugs on app not showing comps

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -19,32 +19,32 @@ export function Exercise() {
   }
 
   return (
-    <ScrollView>
-      <VStack flex={1}>
-        <VStack px={8} bg="gray.600" pt={12}>
-          <TouchableOpacity onPress={handleGoBack}>
-            <Icon
-              as={Feather}
-              name="arrow-left"
-              color="green.500"
-              size={6}
-            />
-          </TouchableOpacity>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon
+            as={Feather}
+            name="arrow-left"
+            color="green.500"
+            size={6}
+          />
+        </TouchableOpacity>
 
-          <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
-            <Heading color="gray.100" fontSize="lg" flexShrink={1}>
-              Puxada frontal
-            </Heading>
+        <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Puxada frontal
+          </Heading>
 
-            <HStack alignItems="center">
-              <BodySvg />
+          <HStack alignItems="center">
+            <BodySvg />
 
-              <Text color="gray.200" ml={1} textTransform="capitalize">
-                Costas
-              </Text>
-            </HStack>
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
           </HStack>
-        </VStack>
+        </HStack>
+      </VStack>
+      <ScrollView>
 
         <VStack p={8}>
           <Image
@@ -79,7 +79,7 @@ export function Exercise() {
             />
           </Box>
         </VStack>
-      </VStack>
-    </ScrollView>
+      </ScrollView>
+    </VStack >
   );
 }
