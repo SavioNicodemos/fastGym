290e57c0338d5c6f9614ae6b70dc8e38766d7376
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-27 20:42:43 +0100

    Added groups to a FlatList

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,28 +1,33 @@
 import { useState } from 'react';
-import { HStack, VStack } from 'native-base';
+import { HStack, VStack, FlatList } from 'native-base';
+
 import { Group } from '@components/Groups';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro']);
   const [groupSelected, setGroupSelected] = useState('costa');
 
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <HStack>
-        <Group
-          name='costa'
-          isActive={groupSelected === 'costa'}
-          onPress={() => setGroupSelected('costa')}
-        />
-
-        <Group
-          name='ombro'
-          isActive={groupSelected === 'ombro'}
-          onPress={() => setGroupSelected('ombro')}
-        />
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) =>
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        }
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
