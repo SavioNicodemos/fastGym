cdf3faf528f2134ea898531063a954ba6ad2cde3
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-27 12:20:37 +0100

    Added custom theme in NativeBase

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { NativeBaseProvider, Box } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         style='light'
         backgroundColor='transparent'
