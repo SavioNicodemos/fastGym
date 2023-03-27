2a68ced810b1465161f7a6b9b67ead7d8d7e5818
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-27 21:46:02 +0100

    Finished Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -16,7 +16,7 @@ export function Profile() {
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
 
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {
             photoIsLoading ?
@@ -52,10 +52,7 @@ export function Profile() {
             isDisabled
           />
 
-        </Center>
-
-        <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}>
             Alterar senha
           </Heading>
 
@@ -78,7 +75,7 @@ export function Profile() {
           />
 
           <Button title="Atualizar" mt={4} />
-        </VStack>
+        </Center>
       </ScrollView>
     </VStack>
   );
