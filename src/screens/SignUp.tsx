ca3540c81c403d9b842a1bf3cd3254eb3fcf2c22
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-28 16:58:31 +0100

    Using axios and handling exceptions

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,14 +1,17 @@
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import axios from "axios";
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { api } from "@services/api";
 
 type FormDataProps = {
   name: string;
@@ -36,8 +39,16 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm });
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post('users', { name, email, password });
+
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data);
+      }
+    }
   }
 
   return (
