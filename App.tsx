79790c6e5fc43a20df4c67c89925905f9c0f4123
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-28 21:26:08 +0100

    Rename AuthContext

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { AuthContextProvider } from '@contexts/Authcontext';
+import { AuthContextProvider } from '@contexts/AuthContext';
 
 import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
