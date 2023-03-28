bc3e5401639299ed0ea87c5e57bdc9086311eafc
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-28 21:42:44 +0100

    Loading while user data is not ready

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,25 @@
-import { Box, useTheme } from 'native-base';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useContext } from 'react';
+
+import { useTheme, Box } from 'native-base';
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 import { useAuth } from '@hooks/useAuth';
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
+import { Loading } from '@components/Loading';
 
 export function Routes() {
-  const nativeBaseTheme = useTheme();
+  const { colors } = useTheme();
 
-  const { user } = useAuth();
+  const { user, isLoadingUserStorageData } = useAuth();
 
   const theme = DefaultTheme;
-  theme.colors.background = nativeBaseTheme.colors.gray[700];
+  theme.colors.background = colors.gray[700];
+
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg="gray.700">
@@ -20,5 +27,5 @@ export function Routes() {
         {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
-  )
+  );
 }
