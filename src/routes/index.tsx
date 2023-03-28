8f44018f17a75aa17bcee6cb71de14db344e8e2f
Nicodemos Santos <nicodemosgcosta@gmail.com>
2023-03-28 19:31:24 +0100

    Created custom hook to share data

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { Box, useTheme } from 'native-base';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+
+import { useAuth } from '@hooks/useAuth';
+
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const nativeBaseTheme = useTheme();
+
+  const { user } = useAuth();
+  console.log("Usuário logado =>", user);
 
   const theme = DefaultTheme;
   theme.colors.background = nativeBaseTheme.colors.gray[700];
