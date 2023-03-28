08cc3ded5cf0a2e5c894983de95e46f97447234d
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-28 18:18:08 +0100

    Add interceptors to Axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: "http://192.168.1.66:3333",
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { api };
