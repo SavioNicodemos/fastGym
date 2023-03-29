0ab90b3a5c7746216559dde6af74aefd065e5df4
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-29 21:53:42 +0100

    Update baseUrl form axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -18,7 +18,7 @@ type APIInstanceProps = AxiosInstance & {
 };
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://192.168.1.66:3333",
 }) as APIInstanceProps;
 
 let failedQueued: Array<PromiseType> = [];
